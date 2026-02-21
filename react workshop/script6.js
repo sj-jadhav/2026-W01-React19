6868359a3ae9760a60592442d3b27de89255e219
Shubham Jadhav <shubhamjadhav1239@gmail.com>
2026-02-21 14:32:42 +0530

    react

--- react workshop/script6.js
+++ react workshop/script6.js
@@ -1,13 +1,15 @@
 let employees ={
     name : "smit devid",
     email:"smit1234@gmail.com",
-    skills:["java","SQL","python","azure"],
+    skills:["java","SQL","python","azure","html"],
     role:[
         {company :"wipro",title:"architecht"},
-        {comapany:"LTS" , title:"analyst"}
+        {comapany:"LTS" , title:"analyst"},
     ]
     
 
 }
-let _skills= employees.skills;
-console.log(_skills.slice(-3));
+let _skills={...employees.skills};
+console.log(_skills);
+employees={...employees,skills :[...employees.skills,"react"]};
+console.log(employees);
